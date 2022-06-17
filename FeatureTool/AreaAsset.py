7579277d3b78126a263dcf83f18de14ba2001146
MaddHatt-PM <MaddHatt.pm@gmail.com>
2022-06-17 11:57:56 -0400

    Added API request tracking and archiving. Moved area specific settings to json. Started work on InspectorUI

--- FeatureTool/AreaAsset.py
+++ FeatureTool/AreaAsset.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import json
 from pathlib import Path
 from tkinter import Canvas, Frame
 from turtle import pos
@@ -8,7 +9,15 @@ from matplotlib.pyplot import fill
 from CanvasDrawers import canvas_util
 from InspectorDrawers import inspector_drawers
 from LoadedAsset import loaded_asset
-from Utilities import coord_mode, ui_colors
+from Utilities import color_set, coord_mode, ui_colors
+
+
+class settings_keys:
+    fill_alpha = "fill_alpha"
+    stroke_width = "stroke_width"
+    color = "color"
+    _color_path = "color_path"
+    _color_fill = "color_fill"
 
 class area_asset:    
     def __init__(self, name:str, target:loaded_asset) -> None:
@@ -26,16 +35,25 @@ class area_asset:
 
         basepath = "SavedAreas/" + target.savename + "/" 
 
+
+        self._settings_path = Path(basepath + name + "_settings.txt")
+        if self._settings_path.is_file():
+            with self._settings_path.open('r') as file:
+                self.settings:dict = json.loads(file.read())
+                
+                path = self.settings.pop(settings_keys._color_path)
+                fill = self.settings.pop(settings_keys._color_fill)
+                self.settings[settings_keys.color] = color_set(path, fill)
+        else:
+            self.settings = {}
+            self.settings[settings_keys.fill_alpha] = 0.25
+            self.settings[settings_keys.stroke_width] = 3.0
+            self.settings[settings_keys.color] = ui_colors.indigo
+            self.__save_settings()
+
         self.fill_alpha = 0.25
         self.stroke_width = 3.0
         self.color = ui_colors.indigo
-
-        self._settings_filepath = Path(basepath + name + "_prop.txt")
-        if self._settings_filepath.is_file():
-            with open(str(self._settings_filepath), 'r') as file:
-                # do later
-                pass
-
 
         self._stroke_filepath = Path(basepath + name + "_area.csv")
         self.coord_id:coord_mode = coord_mode.normalized
@@ -45,42 +63,38 @@ class area_asset:
             with open(str(self._stroke_filepath), 'r') as file:
                 lines = file.read().splitlines()
                 
-
                 for id, line in enumerate(lines):
                     if id == 0:
-                        # Interprete csv header to get coordinate type
-                        if "normalized" in lines[0]:
-                            self.coord_id = coord_mode.normalized
-                        elif "longitude" in lines[0]:
-                            self.coord_id = coord_mode.earth
-                        elif "pixel" in lines[0]:
-                            self.coord_id = coord_mode.pixel
+                        continue
 
-                    else:
-                        # Convert nonheader lines to tuple
-                        # and add to the raw data
-                        self.stroke_data.append(eval(line))
-
+                    # Convert nonheader lines to tuple
+                    # and add to the raw data
+                    self.stroke_data.append(eval(line))
 
         self.fill_img:Image = None
-        self._filled_filepath = Path(basepath + name + "_fill.tif")
-        if self._filled_filepath.is_file():
-            self.fill_img = Image.open(self._filled_filepath)
+        self._fill_img_filepath = Path(basepath + name + "_fill.tif")
+        if self._fill_img_filepath.is_file():
+            self.fill_img = Image.open(self._fill_img_filepath)
+
+    def __save_settings(self):
+        settings = dict(self.settings)
+        color:color_set = settings.pop(settings_keys.color)
+        settings[settings_keys._color_path] = color.path
+        settings[settings_keys._color_fill] = color.fill
+
+        with self._settings_path.open('w') as file:
+            # color_set is not json compatible
+            settings_str = json.dumps(settings)
+            file.write(settings_str)
         
-    def write_data_to_files(self):
-        if self.is_dirty == False:
-            return
+    def save_data_to_files(self):
+        # if self.is_dirty == False:
+        #     return
+
+        with self._stroke_filepath.open('r') as file:
+            header = file.readline()
 
         with open(str(self._stroke_filepath), 'w') as file:
-            # Write header
-            header = "error_x,error_y"
-            if self.coord_id == coord_mode.normalized:
-                header = "normalized_x,normalized_y"
-            elif self.coord_id == coord_mode.pixel:
-                header = "pixel_x,pixel_y"
-            elif self.coord_id == coord_mode.earth:
-                header = "latitude,longitude"
-            
             file.write(header + '\n')
 
             # Write stroke data
@@ -257,7 +271,7 @@ class area_asset:
             drawer.polygon(bgm_xy_coords, fill="black")
             drawer.polygon(polygon_points, fill="white")
 
-            fill_img.save("TEST.png")
+            fill_img.save(self._fill_img_filepath)
             self.fill_img = fill_img
             
 
@@ -281,6 +295,9 @@ class area_asset:
         header = self.drawer.header(text=self.name)
         ids.append(header)
         ids.append(self.drawer.seperator())
+
+        header = self.drawer.header(text="Settings")
+        
 
         fill_btn = self.drawer.button(text="Toggle Fill", command=self.toggle_fill)
         ids.append(fill_btn)
