7de7a91edc28a476b65b595cc7398a1ab2aa830a
MaddHatt-PM <MaddHatt.pm@gmail.com>
2022-06-28 17:52:47 -0400

    weird quirk with utilites file

--- FeatureTool/utilities__.py
+++ FeatureTool/utilities__.py
@@ -41,7 +41,7 @@ class SpaceTransformer:
         self.image_raw = image_raw
         self.geo_util:Geodesic = Geodesic.WGS84
 
-    def point_to_size_coords(self, pt, size=8, addOffset=False) -> tuple:
+    def point_to_size_coords(self, pt:tuple[float,float], size=8, addOffset=False) -> tuple:
         '''pt is assumed to be the centered position point'''
         if addOffset is True:
             offset = self.canvasRef.canvasx(0), self.canvasRef.canvasy(0)
@@ -55,7 +55,7 @@ class SpaceTransformer:
 
         return (x0, y0, x1, y1)
 
-    def earth_pt_to_pixel_space(self, pt, to_int=False) -> tuple:
+    def earth_pt_to_pixel_space(self, pt:tuple[float,float], to_int=False) -> tuple:
         '''
         Convert an earth point (lat, lon) to 0-1 normalization
         then to pixel coordinates.
@@ -75,7 +75,7 @@ class SpaceTransformer:
 
         return self.norm_pt_to_pixel_space(norm_x, norm_y)
 
-    def pixel_pt_to_earth_space(self, pt) -> tuple:
+    def pixel_pt_to_earth_space(self, pt:tuple[float,float]) -> tuple:
         '''
         Convert a pixel point to 0-1 normalization
         then to earth coordinates (lat, long).
@@ -83,7 +83,7 @@ class SpaceTransformer:
         pt = self.pixel_pt_to_norm_space(pt)
         return self.norm_pt_to_earth_space(pt)
 
-    def pixel_pt_to_norm_space(self, pt) -> tuple:
+    def pixel_pt_to_norm_space(self, pt:tuple[float,float]) -> tuple:
         min_x = self.canvasRef.canvasx(0)
         max_x = self.image_raw.width
         min_y = self.canvasRef.canvasy(0)
@@ -95,7 +95,7 @@ class SpaceTransformer:
         return norm_x, norm_y
 
 
-    def norm_pt_to_pixel_space(self, pt, to_int=False) -> tuple:
+    def norm_pt_to_pixel_space(self, pt:tuple[float,float], to_int=False) -> tuple:
         x = pt[0] * self.image_raw.width
         y = pt[1] * self.image_raw.height
         
@@ -105,7 +105,7 @@ class SpaceTransformer:
 
         return (x, y)
 
-    def norm_pt_to_earth_space(self, pt) -> tuple:
+    def norm_pt_to_earth_space(self, pt:tuple[float,float]) -> tuple:
         coords = self.target.coordinates()
         max_x = coords[1][0]
         min_x = coords[0][0]
@@ -117,7 +117,7 @@ class SpaceTransformer:
 
         return (x, y)
 
-    def latlong_lerp(self, pt0, pt1, value) -> tuple[float, float]:
+    def latlong_lerp(self, pt0:tuple[float,float], pt1:tuple[float,float], value) -> tuple[float, float]:
         geo = Geodesic.WGS84
 
         return 0.0, 0.0
