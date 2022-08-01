d0b52c9fc43ee7b78a554c0359f4086876eab11e
MaddHattPatt <MaddHatt.pm@gmail.com>
2022-07-31 20:16:09 -0400

    Seperated out elevation representations into multiple file outputs

--- FeatureTool/util_elevation.py
+++ FeatureTool/util_elevation.py
@@ -8,90 +8,104 @@ import numpy as np
 from scipy.interpolate import griddata
 import matplotlib
 import api_keys as keys
+from asset_area import ProjectAsset
 
-coordpath = Path('SavedAreas\AshevilleClub\Coordinates.csv')
-with coordpath.open('r') as file:
-    lines = file.read().splitlines()
-    headers = lines.pop(0).split(',')
-    NW = eval(lines[0])
-    SE = eval(lines[1])
+def generate_imagery(target: ProjectAsset):
+    with target.coordinates_path.open('r') as file:
+        lines = file.read().splitlines()
+        headers = lines.pop(0).split(',')
+        NW = eval(lines[0])
+        SE = eval(lines[1])
 
-datapath =  Path('SavedAreas\AshevilleClub\Elevation.csv')
-with datapath.open('r') as file:
-    lines = file.read().splitlines()
-    headers = lines.pop(0).split(',')
+    with target.elevationCSV_path.open('r') as file:
+        lines = file.read().splitlines()
+        headers = lines.pop(0).split(',')
 
-latID = headers.index('latitude')
-lonID = headers.index('longitude')
-eleID = headers.index('elevation')
-x,y = [],[]
-xy, z = [], []
-for ln in lines:
-    raw = ln.split(',')
-    pt = [eval(pt) for pt in raw]
-    x.append(pt[lonID])
-    y.append(pt[latID])
-    xy.append((pt[lonID], pt[latID]))
-    z.append(pt[eleID])
+    latID = headers.index('latitude')
+    lonID = headers.index('longitude')
+    eleID = headers.index('elevation')
+    x,y = [],[]
+    xy, z = [], []
+    for ln in lines:
+        raw = ln.split(',')
+        pt = [eval(pt) for pt in raw]
+        x.append(pt[lonID])
+        y.append(pt[latID])
+        xy.append((pt[lonID], pt[latID]))
+        z.append(pt[eleID])
 
-x = np.array(x)
-y = np.array(y)
-xy = np.array(xy)
-z = np.array(z)
+    x = np.array(x)
+    y = np.array(y)
+    xy = np.array(xy)
+    z = np.array(z)
 
-range_x = [NW[1], SE[1]]
-range_y = [NW[0], SE[0]]
+    range_x = [NW[1], SE[1]]
+    range_y = [NW[0], SE[0]]
 
-grid_x,grid_y = np.mgrid[
-    min(range_x):max(range_x):1000j,
-    min(range_y): max(range_y):1000j
-]
+    grid_x,grid_y = np.mgrid[
+        min(range_x):max(range_x) :5000j,
+        min(range_y): max(range_y):5000j
+    ]
 
-height_data = griddata(xy, z, (grid_x, grid_y), method='linear')
+    def resize_to_satelite(img_path:Path):
+        img = Image.open(img_path)
+        img = img.resize(Image.open(target.sateliteImg_path).size)
+        img.save(img_path)
 
-height = NW[0] - SE[0]
-width = SE[1] - NW[1]
-aspect = width / height
-multiplier = 8000
-plt.figure(figsize= (width * multiplier, height * multiplier))
-print((width * multiplier, height * multiplier))
+    '''Save out data point distribution'''
+    print(Image.open(target.sateliteImg_path).info['dpi'])
 
-plt.plot(x, y, 'k.', ms=1)
+    height = NW[0] - SE[0]
+    width = SE[1] - NW[1]
+    multiplier = 10000
+    
+    plt.figure(figsize= (width * multiplier, height * multiplier))
+    plt.plot(x, y, 'r.', ms=1)
+    plt.axis('off')
+    plt.savefig(target.datapointImg_path, bbox_inches='tight', pad_inches=0, transparent=True)
+    plt.clf()
 
-LEVELS = 50
-plt.tricontour(x, y, z, levels=LEVELS, cmap='inferno')
+    resize_to_satelite(target.datapointImg_path)
 
-contour_path = Path('contour_buffer.png')
-plt.axis('off')
-plt.savefig(contour_path, bbox_inches='tight', pad_inches=0, transparent=True)
-plt.clf()
+    '''Save out gradient maps in all three interpolation methods'''
+    def plot_gradmap(config:tuple[str, str]):
+        interpolation, filepath = config
+        height_data = griddata(xy, z, (grid_x, grid_y), method=interpolation)
 
-plt.imshow(
-    height_data,
-    extent=(SE[1], NW[1], SE[0], NW[0]),
-    origin='lower',
-    cmap='cubehelix')
+        plt.imshow(
+            height_data,
+            extent=(SE[1], NW[1], SE[0], NW[0]),
+            origin='lower',
+            cmap='cubehelix')
 
-# plt.show()
-height_path = Path('example.png')
-plt.axis('off')
-plt.savefig(height_path, bbox_inches='tight', pad_inches=0, transparent=True)
+        plt.axis('off')
+        plt.savefig(filepath, bbox_inches='tight', pad_inches=0, transparent=True)
 
-con_fig = Image.open(contour_path)
-# con_fig = con_fig.rotate(180)
-# con_fig.
-con_fig.save(contour_path)
+        fig_img = Image.open(filepath)
+        fig_img = fig_img.rotate(90, expand=True)
+        fig_img = fig_img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+        fig_img = fig_img.resize((fig_img.size[1], fig_img.size[0]))
+        fig_img.save(filepath)
 
-fig_img = Image.open(height_path)
-fig_img = fig_img.rotate(90, expand = True)
-fig_img = fig_img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-fig_img = fig_img.resize((fig_img.size[1], fig_img.size[0]))
-print(fig_img.size)
+        resize_to_satelite(filepath)
 
-ref_img = Image.open(Path('SavedAreas\AshevilleClub\Satelite.png'))
-print(ref_img.size)
+    grad_configs = [
+        ('nearest', target.elevationImg_nearest_path),
+        ('linear', target.elevationImg_linear_path),
+    ]
 
-fig_img.paste(con_fig, (0,0), con_fig)
+    for config in grad_configs:
+        plot_gradmap(config)
 
-fig_img = fig_img.resize(ref_img.size)
-fig_img.save(height_path)
+    '''Save out contour map'''
+    LEVELS = 50
+    plt.clf()
+    plt.tricontour(x, y, z, levels=LEVELS, cmap='inferno')
+    plt.axis('off')
+    plt.savefig(target.contourImg_path, bbox_inches='tight', pad_inches=0, transparent=True)
+    resize_to_satelite(target.contourImg_path)
+
+
+if __name__ == '__main__':
+    target = ProjectAsset('AshevilleClub')
+    generate_imagery(target)
