62f0390b82b642afb95a042c7b21bb02d0691239
MaddHatt-PM <MaddHatt.pm@gmail.com>
2022-08-03 22:53:12 -0400

    Difference of elevation is now rendered as a seperate graph. Asthetic changes to the graphs.

--- FeatureTool/view_compare_data.py
+++ FeatureTool/view_compare_data.py
@@ -7,27 +7,44 @@ from data_downloader import download_elevation, services
 from utilities import meter_to_feet
 
 def __generate_plot(datasets:list[list], labels:list[str]):
+    title = 'Elevation Comparison between GPS App and Google Maps'
+    plt.figure(title)
+
     plt.subplot(2,1,1)
     for index, ds in enumerate(datasets):
         plt.plot([*range(len(ds))], ds, label=labels[index])
 
-    plt.fill_between([*range(len(ds))], *datasets, color='grey', alpha=0.15)
-    plt.title('asdf')
+    plt.fill_between([*range(len(ds))], *datasets, color='grey', alpha=0.2)
+    plt.title(title)
     plt.xlabel('Sample Point Index')
-    plt.ylabel('Elevation(m) from sealevel')
+    plt.ylabel('Elevation(ft) from sealevel')
     plt.legend(loc='upper right')
+    plt.margins(x=0)
     plt.grid()
 
-    
-    plt.subplot(2,1,2)
+    ax = plt.subplot(2,1,2)
     diff = []
     for index in range(len(datasets[0])):
         diff.append(datasets[0][index] - datasets[1][index])
+
+    x0, y0 = [0, len(datasets[0])], [0, 0]
+    plt.plot(x0, y0, color='k')
     
-    plt.plot([*range(len(diff))], diff)
+    plt.plot([*range(len(diff))], diff, label='GPS Elev. minus Google Elev.', color='tab:purple')
+    plt.fill_between([*range(len(diff))], diff, color='grey', alpha=0.2)
+    plt.title('Difference of Elevation')
     plt.xlabel('Sample Point Index')
-    plt.ylabel('Difference Elevation(m)')
+    plt.ylabel('Difference Elevation(ft)')
+    plt.legend(loc='upper right')
+    plt.margins(x=0)
     plt.grid()
+
+    plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.3)
 
     plt.show()
 
@@ -54,7 +71,7 @@ if __name__ == '__main__':
         walking_altitude_pts.append(eval(ln[altitude_id]))
 
     '''Google Elevation (API Request Per Point)'''
-    legend_labels.append('Google Elevation (API Request Per Point')
+    legend_labels.append('Google Elevation (API Request Per Point)')
     output_path = Path('temp.csv')
     if output_path.exists() is False:
         output_path = download_elevation(target=None, points=latlong_pts, service=services.google_elevation , output_path=output_path, sample_inside_polygon=False)
@@ -69,6 +86,9 @@ if __name__ == '__main__':
     for ln in lines:
         ln = ln.split(',')
         google_altitude_pts.append(eval(ln[altitude_id]))
+
+    walking_altitude_pts = [meter_to_feet(pt) for pt in walking_altitude_pts]
+    google_altitude_pts = [meter_to_feet(pt) for pt in google_altitude_pts]
 
     __generate_plot(
         datasets= [walking_altitude_pts, google_altitude_pts],
